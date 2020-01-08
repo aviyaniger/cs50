c97dec033a219198612e08d7e5502b291f6022ff
aviyaniger <aviyaniger@users.noreply.github.com>
2020-01-08 17:29:05 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -10,16 +10,19 @@ int main(void)
     do
     {
         height = get_int("Height: ");
-    } while (height < 1 || height > 8);
+    }
+    while (height < 1 || height > 8);
 
     //create pyramid
     for (int i = 0; i < height; i++)
     {
+        //print spaces
         space = height - i - 1;
         for (int k = 0; k < space; k++)
         {
             printf(" ");
         }
+        //prin hashes
         for (int j = 0; j <= i; j++)
         {
             printf("#");
