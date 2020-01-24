304b277d8c4610a321a314fff011220e9df0b1f5
aviyaniger <aviyaniger@users.noreply.github.com>
2020-01-24 07:42:42 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -86,94 +86,88 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             original_image[i][j] = image[i][j];
         }
     }
-
-    // top left corner
-    int average_red = (int) round((original_image[0][0].rgbtRed + original_image[0][1].rgbtRed + original_image[1][0].rgbtRed + original_image[1][1].rgbtRed) / 4);
-    int average_green = (int) round((original_image[0][0].rgbtGreen + original_image[0][1].rgbtGreen + original_image[1][0].rgbtGreen + original_image[1][1].rgbtGreen) / 4);
-    int average_blue = (int) round((original_image[0][0].rgbtBlue + original_image[0][1].rgbtBlue + original_image[1][0].rgbtBlue + original_image[1][1].rgbtBlue) / 4);
-    image[0][0].rgbtRed = average_red;
-    image[0][0].rgbtGreen = average_green;
-    image[0][0].rgbtBlue = average_blue;
-
-    // top right corner
-    average_red = (int) round((original_image[0][width - 2].rgbtRed + original_image[0][width - 1].rgbtRed + original_image[1][width - 2].rgbtRed + original_image[1][width - 1].rgbtRed) / 4);
-    average_green = (int) round((original_image[0][width - 2].rgbtGreen + original_image[0][width - 1].rgbtGreen + original_image[1][width - 2].rgbtGreen + original_image[1][width - 1].rgbtGreen) / 4);
-    average_blue = (int) round((original_image[0][width - 2].rgbtBlue + original_image[0][width - 1].rgbtBlue + original_image[1][width - 2].rgbtBlue + original_image[1][width - 1].rgbtBlue) / 4);
-    image[0][width - 1].rgbtRed = average_red;
-    image[0][width - 1].rgbtGreen = average_green;
-    image[0][width - 1].rgbtBlue = average_blue;
-
-    // bottom left corner
-    average_red = (int) round((original_image[height - 2][0].rgbtRed + original_image[height - 2][1].rgbtRed + original_image[height - 1][0].rgbtRed + original_image[height - 1][1].rgbtRed) / 4);
-    average_green = (int) round((original_image[height - 2][0].rgbtGreen + original_image[height - 2][1].rgbtGreen + original_image[height - 1][0].rgbtGreen + original_image[height - 1][1].rgbtGreen) / 4);
-    average_blue = (int) round((original_image[height - 2][0].rgbtBlue + original_image[height - 2][1].rgbtBlue + original_image[height - 1][0].rgbtBlue + original_image[height - 1][1].rgbtBlue) / 4);
-    image[height - 1][0].rgbtRed = average_red;
-    image[height - 1][0].rgbtGreen = average_green;
-    image[height - 1][0].rgbtBlue = average_blue;
-
-    // bottom right corner
-    average_red = (int) round((original_image[height - 2][width - 2].rgbtRed + original_image[height - 2][width - 1].rgbtRed + original_image[height - 1][width - 2].rgbtRed + original_image[height - 1][width - 1].rgbtRed) / 4);
-    average_green = (int) round((original_image[height - 2][width - 2].rgbtGreen + original_image[height - 2][width - 1].rgbtGreen + original_image[height - 1][width - 2].rgbtGreen + original_image[height - 1][width - 1].rgbtGreen) / 4);
-    average_blue = (int) round((original_image[height - 2][width - 2].rgbtBlue + original_image[height - 2][width - 1].rgbtBlue + original_image[height - 1][width - 2].rgbtBlue + original_image[height - 1][width - 1].rgbtBlue) / 4);
-    image[height - 1][width - 1].rgbtRed = average_red;
-    image[height - 1][width - 1].rgbtGreen = average_green;
-    image[height - 1][width - 1].rgbtBlue = average_blue;
-
     for (int i = 0; i < height; i++)
     {
-        for (int j  = 0; j < width; j++)
+        for (int j = 0; j < width; j++)
         {
-            // middle
-            if (i > 0 && i < height - 1 && j > 0 && j < width - 1)
+            void add_colors(int *pixRed, int *pixGreen, int *pixBlue, int *red, int *green, int *blue, int *pixnum);
+
+            int pixnum = 0;
+            int red = 0;
+            int green = 0;
+            int blue = 0;
+            if (i-1 >= 0 && j-1 >= 0)
             {
-                int red = (int) round((original_image[i-1][j-1].rgbtRed + original_image[i-1][j].rgbtRed + original_image[i-1][j+1].rgbtRed + original_image[i][j-1].rgbtRed + original_image[i][j].rgbtRed + original_image[i][j+1].rgbtRed + original_image[i+1][j-1].rgbtRed + original_image[i+1][j].rgbtRed + original_image[i+1][j+1].rgbtRed) / 9);
-                int green = (int) round((original_image[i-1][j-1].rgbtGreen + original_image[i-1][j].rgbtGreen + original_image[i-1][j+1].rgbtGreen + original_image[i][j-1].rgbtGreen + original_image[i][j].rgbtGreen + original_image[i][j+1].rgbtGreen + original_image[i+1][j-1].rgbtGreen + original_image[i+1][j].rgbtGreen + original_image[i+1][j+1].rgbtGreen) / 9);
-                int blue = (int) round((original_image[i-1][j-1].rgbtBlue + original_image[i-1][j].rgbtBlue + original_image[i-1][j+1].rgbtBlue + original_image[i][j-1].rgbtBlue + original_image[i][j].rgbtBlue + original_image[i][j+1].rgbtBlue + original_image[i+1][j-1].rgbtBlue + original_image[i+1][j].rgbtBlue + original_image[i+1][j+1].rgbtBlue) / 9);
-                image[i][j].rgbtRed = red;
-                image[i][j].rgbtGreen = green;
-                image[i][j].rgbtBlue = blue;
+                int pixRed = original_image[i-1][j-1].rgbtRed;
+                int pixGreen = original_image[i-1][j-1].rgbtGreen;
+                int pixBlue = original_image[i-1][j-1].rgbtBlue;
+                add_colors(&pixRed, &pixGreen, &pixBlue, &red, &green, &blue, &pixnum);
             }
-            // top
-            else if (i == 0 && j > 0 && j < width - 1)
+            if (i-1 >= 0)
             {
-                int red = (int) round((original_image[i][j-1].rgbtRed + original_image[i][j].rgbtRed + original_image[i][j+1].rgbtRed + original_image[i+1][j-1].rgbtRed + original_image[i+1][j].rgbtRed + original_image[i+1][j+1].rgbtRed) / 6);
-                int green = (int) round((original_image[i][j-1].rgbtGreen + original_image[i][j].rgbtGreen + original_image[i][j+1].rgbtGreen + original_image[i+1][j-1].rgbtGreen + original_image[i+1][j].rgbtGreen + original_image[i+1][j+1].rgbtGreen) / 6);
-                int blue = (int) round((original_image[i][j-1].rgbtBlue + original_image[i][j].rgbtBlue + original_image[i][j+1].rgbtBlue + original_image[i+1][j-1].rgbtBlue + original_image[i+1][j].rgbtBlue + original_image[i+1][j+1].rgbtBlue) / 6);
-                image[i][j].rgbtRed = red;
-                image[i][j].rgbtGreen = green;
-                image[i][j].rgbtBlue = blue;
+                int pixRed = original_image[i-1][j].rgbtRed;
+                int pixGreen = original_image[i-1][j].rgbtGreen;
+                int pixBlue = original_image[i-1][j].rgbtBlue;
+                add_colors(&pixRed, &pixGreen, &pixBlue, &red, &green, &blue, &pixnum);
             }
-            // bottom
-            else if (i == height - 1 && j > 0 && j < width - 1)
+            if (i-1 >= 0 && j+1 < width)
             {
-                int red = (int) round((original_image[i-1][j-1].rgbtRed + original_image[i-1][j].rgbtRed + original_image[i-1][j+1].rgbtRed + original_image[i][j-1].rgbtRed + original_image[i][j].rgbtRed + original_image[i][j+1].rgbtRed) / 6);
-                int green = (int) round((original_image[i-1][j-1].rgbtGreen + original_image[i-1][j].rgbtGreen + original_image[i-1][j+1].rgbtGreen + original_image[i][j-1].rgbtGreen + original_image[i][j].rgbtGreen + original_image[i][j+1].rgbtGreen) / 6);
-                int blue = (int) round((original_image[i-1][j-1].rgbtBlue + original_image[i-1][j].rgbtBlue + original_image[i-1][j+1].rgbtBlue + original_image[i][j-1].rgbtBlue + original_image[i][j].rgbtBlue + original_image[i][j+1].rgbtBlue) / 6);
-                image[i][j].rgbtRed = red;
-                image[i][j].rgbtGreen = green;
-                image[i][j].rgbtBlue = blue;
+                int pixRed = original_image[i-1][j+1].rgbtRed;
+                int pixGreen = original_image[i-1][j+1].rgbtGreen;
+                int pixBlue = original_image[i-1][j+1].rgbtBlue;
+                add_colors(&pixRed, &pixGreen, &pixBlue, &red, &green, &blue, &pixnum);
             }
-            // left
-            else if (j == 0 && i > 0 && i < height - 1)
+            if (j-1 >= 0)
             {
-                int red = (int) round((original_image[i-1][j].rgbtRed + original_image[i-1][j+1].rgbtRed + original_image[i][j].rgbtRed + original_image[i][j+1].rgbtRed + original_image[i+1][j].rgbtRed + original_image[i+1][j+1].rgbtRed) / 6);
-                int green = (int) round((original_image[i-1][j].rgbtGreen + original_image[i-1][j+1].rgbtGreen + original_image[i][j].rgbtGreen + original_image[i][j+1].rgbtGreen + original_image[i+1][j].rgbtGreen + original_image[i+1][j+1].rgbtGreen) / 6);
-                int blue = (int) round((original_image[i-1][j].rgbtBlue + original_image[i-1][j+1].rgbtBlue + original_image[i][j].rgbtBlue + original_image[i][j+1].rgbtBlue + original_image[i+1][j].rgbtBlue + original_image[i+1][j+1].rgbtBlue) / 6);
-                image[i][j].rgbtRed = red;
-                image[i][j].rgbtGreen = green;
-                image[i][j].rgbtBlue = blue;
+                int pixRed = original_image[i][j-1].rgbtRed;
+                int pixGreen = original_image[i][j-1].rgbtGreen;
+                int pixBlue = original_image[i][j-1].rgbtBlue;
+                add_colors(&pixRed, &pixGreen, &pixBlue, &red, &green, &blue, &pixnum);
             }
-            // right
-            else if (j == width - 1 && i > 0 && i < height - 1)
+            int current_pixRed = original_image[i][j].rgbtRed;
+            int current_pixGreen = original_image[i][j].rgbtGreen;
+            int current_pixBlue = original_image[i][j].rgbtBlue;
+            add_colors(&current_pixRed, &current_pixGreen, &current_pixBlue, &red, &green, &blue, &pixnum);
+            if (j+1 < width)
             {
-                int red = (int) round((original_image[i-1][j-1].rgbtRed + original_image[i-1][j].rgbtRed + original_image[i][j-1].rgbtRed + original_image[i][j].rgbtRed + original_image[i+1][j-1].rgbtRed + original_image[i+1][j].rgbtRed) / 6);
-                int green = (int) round((original_image[i-1][j-1].rgbtGreen + original_image[i-1][j].rgbtGreen + original_image[i][j-1].rgbtGreen + original_image[i][j].rgbtGreen + original_image[i+1][j-1].rgbtGreen + original_image[i+1][j].rgbtGreen) / 6);
-                int blue = (int) round((original_image[i-1][j-1].rgbtBlue + original_image[i-1][j].rgbtBlue + original_image[i][j-1].rgbtBlue + original_image[i][j].rgbtBlue + original_image[i+1][j-1].rgbtBlue + original_image[i+1][j].rgbtBlue) / 6);
-                image[i][j].rgbtRed = red;
-                image[i][j].rgbtGreen = green;
-                image[i][j].rgbtBlue = blue;
+                int pixRed = original_image[i][j+1].rgbtRed;
+                int pixGreen = original_image[i][j+1].rgbtGreen;
+                int pixBlue = original_image[i][j+1].rgbtBlue;
+                add_colors(&pixRed, &pixGreen, &pixBlue, &red, &green, &blue, &pixnum);
             }
+            if (i+1 < height && j-1 >= 0)
+            {
+                int pixRed = original_image[i+1][j-1].rgbtRed;
+                int pixGreen = original_image[i+1][j-1].rgbtGreen;
+                int pixBlue = original_image[i+1][j-1].rgbtBlue;
+                add_colors(&pixRed, &pixGreen, &pixBlue, &red, &green, &blue, &pixnum);
+            }
+            if (i+1 < height)
+            {
+                int pixRed = original_image[i+1][j].rgbtRed;
+                int pixGreen = original_image[i+1][j].rgbtGreen;
+                int pixBlue = original_image[i+1][j].rgbtBlue;
+                add_colors(&pixRed, &pixGreen, &pixBlue, &red, &green, &blue, &pixnum);
+            }
+            if (i+1 < height && j+1 < width)
+            {
+                int pixRed = original_image[i+1][j+1].rgbtRed;
+                int pixGreen = original_image[i+1][j+1].rgbtGreen;
+                int pixBlue = original_image[i+1][j+1].rgbtBlue;
+                add_colors(&pixRed, &pixGreen, &pixBlue, &red, &green, &blue, &pixnum);
+            }
+            image[i][j].rgbtRed = round(red / pixnum);
+            image[i][j].rgbtGreen = round(green / pixnum);
+            image[i][j].rgbtBlue = round(blue / pixnum);
         }
     }
     return;
+}
+
+void add_colors(int *pixRed, int *pixGreen, int *pixBlue, int *red, int *green, int *blue, int *pixnum)
+{
+    *red += *pixRed;
+    *green += *pixGreen;
+    *blue += *pixBlue;
+    *pixnum += 1;
 }
